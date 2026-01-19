d3502e4b3db5907a9686093f95e8a9625a972140
JeremySu0818 <xinghong.su0818@gmail.com>
2026-01-19 20:38:50 +0800

    refactor: Adjust compilation flags for platform-specific optimization and add Cython directives for performance.

--- setup.py
+++ setup.py
@@ -1,18 +1,29 @@
 from setuptools import setup, Extension
 from Cython.Build import cythonize
+import sys
+
+if sys.platform == "win32":
+    compile_args = ["/O2"]
+else:
+    compile_args = ["-O3"]
 
 ext_modules = [
     Extension(
         "TestingCore",
         sources=["TestingCore.pyx"],
-        extra_compile_args=['-O3', '-march=native'],
+        extra_compile_args=compile_args,
     )
 ]
 
 setup(
-    ext_modules = cythonize(
-        ext_modules, 
+    ext_modules=cythonize(
+        ext_modules,
         annotate=True,
-        compiler_directives={'language_level': "3"}
+        compiler_directives={
+            "language_level": "3",
+            "boundscheck": False,
+            "wraparound": False,
+            "cdivision": True,
+        },
     )
 )
